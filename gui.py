c3bbb83b0c9a594b070197618b19da0549bea743
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-27 01:53:15 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,14 +1,27 @@
 import tkinter as tk
 
-def on_button_click():
-    label.config(text="Button Clicked!")
+def on_button_click(value):
+    current_entry = entry_var.get()
+    
+    if value == 'Enter':
+        check_password(current_entry)
+        entry_var.set('')
+    elif value == 'C':
+        entry_var.set('')
+    else:
+        entry_var.set(current_entry + value)
 
-def on_exit(event):
-    root.destroy()
+def check_password(password_attempt):
+    correct_password = '2003'
+    
+    if password_attempt == correct_password:
+        result_label.config(text="Access Granted", fg="green")
+    else:
+        result_label.config(text="Access Denied", fg="red")
 
 # Create the main window
 root = tk.Tk()
-root.title("My GUI App")
+root.title("Password Entry")
 
 # Set fullscreen mode
 root.attributes("-fullscreen", True)
@@ -19,8 +32,10 @@ root.config(cursor="none")
 # Set a black background
 root.configure(bg="black")
 
-# Create widgets
-label = tk.Label(root, text="Hello, GUI!", fg="white", bg="black", font=("Helvetica", 24))
+# Entry widget to display the password input
+entry_var = tk.StringVar()
+entry_widget = tk.Entry(root, textvariable=entry_var, font=("Helvetica", 20), justify='center', bd=5, relief='solid', fg="white", bg="black")
+entry_widget.pack(pady=20)
 
 # Create a Frame for the keypad
 keypad_frame = tk.Frame(root, bg="black")
@@ -30,12 +45,9 @@ keypad_buttons = [
     '7', '8', '9',
     '4', '5', '6',
     '1', '2', '3',
-    '0', '.', 'Enter'
+    '0', '.', 'Enter',
+    'C'  # Clear button
 ]
-
-# Function to handle keypad button clicks
-def on_keypad_click(value):
-    print(f"Key pressed: {value}")
 
 # Add keypad buttons to the Frame
 for row in range(4):
@@ -43,16 +55,19 @@ for row in range(4):
         index = row * 3 + col
         button_value = keypad_buttons[index]
         button = tk.Button(keypad_frame, text=button_value, width=5, height=2,
-                           command=lambda value=button_value: on_keypad_click(value),
-                           fg="black", bg="white", font=("Helvetica", 16))
+                           command=lambda value=button_value: on_button_click(value),
+                           fg="white", bg="black", bd=4, relief='solid', font=("Helvetica", 16))
         button.grid(row=row, column=col, padx=5, pady=5)
 
+# Label to display access result
+result_label = tk.Label(root, text="", fg="white", bg="black", font=("Helvetica", 20))
+result_label.pack(pady=20)
+
 # Place widgets in a vertical layout
-label.pack(side=tk.TOP, pady=20)
 keypad_frame.pack(side=tk.TOP)
 
-# Bind the Escape key to the exit function
-root.bind("<Escape>", on_exit)
+# Bind the Escape key to exit the application
+root.bind("<Escape>", lambda event: root.destroy())
 
 # Start the main loop
 root.mainloop()
