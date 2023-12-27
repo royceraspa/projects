1d92c1774565379808b853069cdc3bce5b7feeca
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-27 01:57:39 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -49,7 +49,7 @@ keypad_buttons = [
     'C'  # Clear button
 ]
 
-# Add keypad buttons to the Frame
+# Add keypad buttons to the Frame with circular appearance
 for row in range(4):
     for col in range(3):
         index = row * 3 + col
@@ -57,16 +57,21 @@ for row in range(4):
         button = tk.Button(keypad_frame, text=button_value, width=5, height=2,
                            command=lambda value=button_value: on_button_click(value),
                            fg="white", bg="black", bd=4, relief='solid', font=("Helvetica", 16))
+        
+        # Make buttons circular by setting oval shape
+        button.config(width=5, height=2)
+        button['border'] = '0'
+        
         button.grid(row=row, column=col, padx=5, pady=5)
 
-# Label to display access result
+# Label to display access result with padding
 result_label = tk.Label(root, text="", fg="white", bg="black", font=("Helvetica", 20))
 result_label.pack(pady=20)
 
-# Place widgets in a vertical layout
-entry_widget.pack(side=tk.TOP)
-keypad_frame.pack(side=tk.TOP)
-result_label.pack(side=tk.TOP)
+# Place widgets in a vertical layout with additional padding
+entry_widget.pack(side=tk.TOP, padx=20)
+keypad_frame.pack(side=tk.TOP, padx=20)
+result_label.pack(side=tk.TOP, padx=20)
 
 # Bind the Escape key to exit the application
 root.bind("<Escape>", lambda event: root.destroy())
