fb52d85c0de2a75ee6c6d31c360619e18876b399
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-27 03:40:10 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,30 +1,40 @@
-Skip to content
-royceraspa
-/
-projects
-
-Type / to search
-
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-Commit
-Update gui.py
- main
-@royceraspa
-royceraspa committed 1 hour ago 
-1 parent cb9ca1b
-commit 1d92c17
-Showing 1 changed file with 11 additions and 6 deletions.
-  17 changes: 11 additions & 6 deletions17  
-gui.py
-@@ -49,24 +49,29 @@ def check_password(password_attempt):
+import tkinter as tk
+def on_button_click(value):
+    current_entry = entry_var.get()
+    if value == 'Enter':
+        check_password(current_entry)
+        entry_var.set('')
+    elif value == 'C':
+        entry_var.set('')
+    elif len(current_entry) < 4 and value.isdigit():
+        entry_var.set(current_entry + value)
+def check_password(password_attempt):
+    correct_password = '2003'
+    if password_attempt == correct_password:
+        result_label.config(text="ACCESS GRANTED", fg="green")
+    else:
+        result_label.config(text="ACCESS DENIED", fg="red")
+# Create the main window
+root = tk.Tk()
+root.title("Password Entry")
+# Set fullscreen mode
+root.attributes("-fullscreen", True)
+# Hide the mouse cursor
+root.config(cursor="none")
+# Set a black background
+root.configure(bg="black")
+# Entry widget to display the password input
+entry_var = tk.StringVar()
+entry_widget = tk.Entry(root, textvariable=entry_var, font=("Helvetica", 20), justify='center', bd=5, relief='solid', fg="white", bg="black", width=4)
+entry_widget.pack(pady=20)
+# Create a Frame for the keypad
+keypad_frame = tk.Frame(root, bg="black")
+# Define keypad buttons
+keypad_buttons = [
+    '7', '8', '9',
+    '4', '5', '6',
+    '1', '2', '3',
+    '0', 'Enter',
     'C'  # Clear button
 ]
 
@@ -60,22 +70,5 @@ result_label.pack(side=tk.TOP, padx=20)
 
 # Bind the Escape key to exit the application
 root.bind("<Escape>", lambda event: root.destroy())
-0 comments on commit 1d92c17
-@royceraspa
-Comment
- 
-Leave a comment
- 
- You’re receiving notifications because you’re watching this repository.
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact
-Manage cookies
-Do not share my personal information
-Update gui.py · royceraspa/projects@1d92c17
+# Start the main loop
+root.mainloop()
