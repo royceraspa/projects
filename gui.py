062f638a869c17e741ab8848b2c08cc587eb8693
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-27 01:47:01 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -10,13 +10,16 @@ root.title("My GUI App")
 # Set fullscreen mode
 root.attributes("-fullscreen", True)
 
+# Hide the mouse cursor
+root.config(cursor="none")
+
 # Create widgets
 label = tk.Label(root, text="Hello, GUI!")
 button = tk.Button(root, text="Click me!", command=on_button_click)
 
-# Place widgets in the window
-label.pack(pady=20)
-button.pack()
+# Place widgets in a vertical layout
+label.pack(side=tk.TOP, pady=20)
+button.pack(side=tk.TOP)
 
 # Start the main loop
 root.mainloop()
