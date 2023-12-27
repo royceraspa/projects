bd8dfe7f7047f8a499cea88fa214de7efdcf1d89
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-27 01:47:47 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,6 +3,9 @@ import tkinter as tk
 def on_button_click():
     label.config(text="Button Clicked!")
 
+def on_exit(event):
+    root.destroy()
+
 # Create the main window
 root = tk.Tk()
 root.title("My GUI App")
@@ -20,6 +23,9 @@ button = tk.Button(root, text="Click me!", command=on_button_click)
 # Place widgets in a vertical layout
 label.pack(side=tk.TOP, pady=20)
 button.pack(side=tk.TOP)
+
+# Bind the Escape key to the exit function
+root.bind("<Escape>", on_exit)
 
 # Start the main loop
 root.mainloop()
