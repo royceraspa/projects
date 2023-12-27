9ad6868727919ebb85bf556d58de742fd893213e
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-27 03:50:36 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -26,7 +26,7 @@ root.configure(bg="black")
 # Entry widget to display the password input
 entry_var = tk.StringVar()
 entry_widget = tk.Entry(root, textvariable=entry_var, font=("Helvetica", 20), justify='center', bd=5, relief='solid', fg="white", bg="black", width=4)
-entry_widget.pack(pady=50)
+entry_widget.pack(pady=20)
 # Create a Frame for the keypad
 keypad_frame = tk.Frame(root, bg="black")
 # Define keypad buttons
@@ -34,8 +34,8 @@ keypad_buttons = [
     '7', '8', '9',
     '4', '5', '6',
     '1', '2', '3',
-    '0', 'Enter',
-    'C'  # Clear button
+    '0', 'C',
+    'ENTER'  # Clear button
 ]
 
 # Add keypad buttons to the Frame
@@ -47,8 +47,7 @@ for row in range(4):
         button = tk.Button(keypad_frame, text=button_value, width=5, height=2,
                            command=lambda value=button_value: on_button_click(value),
                            fg="white", bg="black", bd=4, relief='solid', font=("Helvetica", 16))
-        button.pack(pady=50)
-        
+
         # Make buttons circular by setting oval shape
         button.config(width=5, height=2)
         button['border'] = '0'
@@ -58,8 +57,16 @@ for row in range(4):
 # Label to display access result
 # Label to display access result with padding
 result_label = tk.Label(root, text="", fg="white", bg="black", font=("Helvetica", 20))
-result_label.pack(pady=-80)
+result_label.pack(pady=20)
 
+# Place widgets in a vertical layout
+entry_widget.pack(side=tk.TOP)
+keypad_frame.pack(side=tk.TOP)
+result_label.pack(side=tk.TOP)
+# Place widgets in a vertical layout with additional padding
+entry_widget.pack(side=tk.TOP, padx=20)
+keypad_frame.pack(side=tk.TOP, padx=20)
+result_label.pack(side=tk.TOP, padx=20)
 
 # Bind the Escape key to exit the application
 root.bind("<Escape>", lambda event: root.destroy())
