f3d00df1835fe60f501bfa06bf8f16f2c94b3faa
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-29 22:29:50 -0500

    Update ir.py

--- ir.py
+++ ir.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define the GPIO pin connected to the IR transmitter
-ir_transmitter_pin = 17  # Change this to the actual GPIO pin you're using
+# Replace with the actual GPIO pin connected to the IR transmitter
+ir_transmitter_pin = 17
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
@@ -10,7 +10,6 @@ GPIO.setup(ir_transmitter_pin, GPIO.OUT)
 
 def send_ir_pulse():
     # Your code here to generate the IR pulse
-    # You may need to use a library like pigpio to precisely control the timing of the pulses
     # Example: GPIO.output(ir_transmitter_pin, GPIO.HIGH)
     #          time.sleep(0.5)  # Adjust the sleep duration based on your requirements
     #          GPIO.output(ir_transmitter_pin, GPIO.LOW)
